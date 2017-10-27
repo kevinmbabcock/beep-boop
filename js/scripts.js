eb7a9e6891a9327aec0cb2e79bb1a6aae0274d5c
Kevin Babcock <kevinmbabcock@icloud.com>
2017-10-27 09:52:12 -0700

    add business logic for numbers containing '1' and '0'

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,15 @@
 var beepBoop = function (number) {
   var range = [];
   for (index = 0; index <= number; index++) {
-    if (index === 0 || index % 10 === 0) {
-      range.push("Beep!");
-    } else if (index === 1 || index % 10 === 1) {
-      range.push("Boop!");
-    } else if (index % 3 === 0) {
+    if (index % 100 === 10) {
+      range.push("Boop-Beep!");
+    } else if (index % 3 === 0 && index !== 0) {
       range.push("I'm sorry, Dave. I'm afraid I can't do that.");
-    }
-    else {
+    } else if (index === 0 || index % 10 === 0) {
+      range.push("Beep!");
+    } else if (index === 1 || index % 10 === 1 || (index > 10 && index < 20)) {
+      range.push("Boop!");
+    } else {
       range.push(index);
     }
   }
@@ -22,7 +23,6 @@ $(document).ready(function() {
     event.preventDefault();
     var userNumber = $("#userNumber").val();
     var result = beepBoop(userNumber);
-    console.log(result);
     $(".result").show();
     $("#output").text(result);
   });
