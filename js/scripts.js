2c4cccebb2b18ef92e90151dde7947b9c03b3a79
Kevin Babcock <kevinmbabcock@icloud.com>
2017-10-27 10:01:57 -0700

    add form for user to input name and display user's name instead of 'Dave' for numbers divisible by 3

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
-var beepBoop = function (number) {
+var beepBoop = function (number, name) {
   var ranges = [];
   for (index = 0; index <= number; index++) {
     if (index % 100 === 10) {
       ranges.push("Boop-Beep!");
     } else if (index % 3 === 0 && index !== 0) {
-      ranges.push("I'm sorry, Dave. I'm afraid I can't do that.");
+      ranges.push("I'm sorry, " + name + ". I'm afraid I can't do that.");
     } else if (index === 0 || index % 10 === 0) {
       ranges.push("Beep!");
     } else if (index === 1 || index % 10 === 1 || (index > 10 && index < 20)) {
@@ -22,11 +22,11 @@ $(document).ready(function() {
   $("#userInput").submit(function(event) {
     event.preventDefault();
     var userNumber = parseInt($("#userNumber").val());
-    var results = beepBoop(userNumber);
+    var userName = $("#userName").val();
+    var results = beepBoop(userNumber, userName);
     results.forEach(function(item) {
       $("#output").append("<li>" + item + "</li>");
     });
     $(".result").show();
-    // $("#output").text(results);
   });
 });
