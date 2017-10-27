78896495db23e9b2ff9f39282b15441ac15339d9
Kevin Babcock <kevinmbabcock@icloud.com>
2017-10-27 10:19:00 -0700

    add alert if user leaves a form blank

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,21 @@
 var beepBoop = function (number, name) {
   var ranges = [];
-  for (index = 0; index <= number; index++) {
-    if (index % 100 === 10) {
-      ranges.push("Boop-Beep!");
-    } else if (index % 3 === 0 && index !== 0) {
-      ranges.push("I'm sorry, " + name + ". I'm afraid I can't do that.");
-    } else if (index === 0 || index % 10 === 0) {
-      ranges.push("Beep!");
-    } else if (index === 1 || index % 10 === 1 || (index > 10 && index < 20)) {
-      ranges.push("Boop!");
-    } else {
-      ranges.push(index);
+  
+  if (!name || !number) {
+    alert("Please follow directions. They are very simple");
+  } else {
+    for (index = 0; index <= number; index++) {
+      if (index % 100 === 10) {
+        ranges.push("Boop-Beep!");
+      } else if (index % 3 === 0 && index !== 0) {
+        ranges.push("I'm sorry, " + name + ". I'm afraid I can't do that.");
+      } else if (index === 0 || index % 10 === 0) {
+        ranges.push("Beep!");
+      } else if (index === 1 || index % 10 === 1 || (index > 10 && index < 20)) {
+        ranges.push("Boop!");
+      } else {
+        ranges.push(index);
+      }
     }
   }
   return ranges;
@@ -24,8 +29,8 @@ $(document).ready(function() {
     var userNumber = parseInt($("#userNumber").val());
     var userName = $("#userName").val();
     var results = beepBoop(userNumber, userName);
-    results.forEach(function(item) {
-      $("#output").append("<li>" + item + "</li>");
+    results.forEach(function(result) {
+      $("#output").append("<li>" + result + "</li>");
     });
     $(".result").show();
   });
