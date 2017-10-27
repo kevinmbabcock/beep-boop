7127869115533ff2557def253477b2af3c04146e
Kevin Babcock <kevinmbabcock@icloud.com>
2017-10-27 10:33:46 -0700

    add radio button to let user choose how numbers are displayed

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,27 @@
-var beepBoop = function (number, name) {
+var beepBoop = function (number, name, direction) {
   var ranges = [];
-  
+
   if (!name || !number) {
-    alert("Please follow directions. They are very simple");
-  } else {
+    alert("Please follow the directions. They are very simple");
+  } else if (direction === "lowFirst" || !direction) {
     for (index = 0; index <= number; index++) {
       if (index % 100 === 10) {
         ranges.push("Boop-Beep!");
+      } else if (index % 3 === 0 && index !== 0) {
+        ranges.push("I'm sorry, " + name + ". I'm afraid I can't do that.");
+      } else if (index === 0 || index % 10 === 0) {
+        ranges.push("Beep!");
+      } else if (index === 1 || index % 10 === 1 || (index > 10 && index < 20)) {
+        ranges.push("Boop!");
+      } else {
+        ranges.push(index);
+      }
+    }
+  } else if (direction === "highFirst") {
+    for (index = number; index >= 0; index--) {
+      if (index % 100 === 10) {
+        ranges.push("Boop-Beep!");
+        //console.log(ranges);
       } else if (index % 3 === 0 && index !== 0) {
         ranges.push("I'm sorry, " + name + ". I'm afraid I can't do that.");
       } else if (index === 0 || index % 10 === 0) {
@@ -28,10 +43,13 @@ $(document).ready(function() {
     event.preventDefault();
     var userNumber = parseInt($("#userNumber").val());
     var userName = $("#userName").val();
-    var results = beepBoop(userNumber, userName);
+    var direction = $("input:radio[name=direction]:checked").val();
+    console.log(direction);
+    var results = beepBoop(userNumber, userName, direction);
     results.forEach(function(result) {
       $("#output").append("<li>" + result + "</li>");
     });
     $(".result").show();
+    $(".number").text(userNumber);
   });
 });
